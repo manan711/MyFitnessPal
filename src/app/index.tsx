2482090bc05af16f44a931110ff60bdc54f35afb
Manan Patel <mananpatel.711@gmail.com>
2024-03-10 18:22:28 -0400

    Food List Item.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import FoodListItem from '../components/FoodListItem';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
-      <StatusBar style="auto" />
+      {/* Food item view */}
+      <FoodListItem item={{label: "Pizza", cal: 75, brand: "Dominos"}}/>
+      <FoodListItem item={{label: "Apple", cal: 50, brand: "Generic"}}/>
     </View>
   );
 }
@@ -16,5 +17,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
+    gap: 5
   },
 });
